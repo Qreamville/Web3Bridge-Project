bafd0333ec37e4e6df8ad46703468e121aa9ea76
Qreamville <Oyeleyebalikis@yahoo.com>
2022-08-22 05:39:23 -1200

    Update hardhat.config.js

--- TodoList/hardhat.config.js
+++ TodoList/hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_RPC_URL || "",
+      accounts: [process.env.PRIVATE_KEY || ""],
+      chainId: 5,
+    },
+  },
   solidity: "0.8.9",
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || "",
+  },
 };
