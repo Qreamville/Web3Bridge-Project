038bb9b3f9e20f9a2e2b433b7de4ba0953faf81b
Qreamville <Oyeleyebalikis@yahoo.com>
2022-08-22 09:12:39 -1200

    Update deploy.js

--- TodoList/scripts/deploy.js
+++ TodoList/scripts/deploy.js
@@ -2,11 +2,21 @@ const { ethers } = require("hardhat");
 
 async function main() {
   const TodoList = await ethers.getContractFactory("TodoList");
+  console.log("Deploying contract | / | ");
   const todoList = await TodoList.deploy();
 
   await todoList.deployed();
 
   console.log(`deployed to ${todoList.address}`);
+
+  await todoList.createTask("Wash plates");
+  console.log(await todoList.getTask(0));
+
+  await todoList.completedTask(0);
+  console.log(await todoList.getTask(0));
+
+  await todoList.editTask(0, "Wash plates and Park the cars");
+  console.log(await todoList.getTask(0));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
