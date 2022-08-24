1593336573ce1e7a344f7560351a44d77a5715c7
Qreamville <Oyeleyebalikis@yahoo.com>
2022-08-24 01:13:59 -1200

    Update deploy.ts

--- Auction/scripts/deploy.ts
+++ Auction/scripts/deploy.ts
@@ -1,18 +1,14 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const itemAmount = ethers.utils.parseEther("1");
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const Auction = await ethers.getContractFactory("Auction");
+  const auction = await Auction.deploy("Estate", itemAmount);
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await auction.deployed();
 
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  console.log(`deployed to ${auction.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
